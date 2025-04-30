ec219b54cf66f91f1b6606574c058874771c689c
natanfudge <natan.lifsiz@gmail.com>
2025-04-30 18:40:55 +0300

    Configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    id ("com.adarshr.test-logger") version "4.0.0"
+    id("com.adarshr.test-logger") version "4.0.0"
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
 
 repositories {
@@ -30,8 +32,8 @@ kotlin {
         nodejs()
     }
     mingwX64()
-//    macosX64()
-//    iosArm64()
+    macosX64()
+    iosArm64()
     linuxX64()
 
 
@@ -43,8 +45,37 @@ kotlin {
         }
     }
 }
-
-
-
 group = "io.github.natanfudge"
 version = "0.1.0"
+
+val artifactId = "wgpu4k-matrix"
+val githubUrl = "https://github.com/natanfudge/wgpu4k-matrix"
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates(group.toString(), artifactId, version.toString())
+    pom {
+        name = artifactId
+        description = "Basic math primitives for WebGPU + Kotlin Multiplatform"
+        inceptionYear = "2025"
+        url = githubUrl
+        licenses {
+            license {
+                name = "The MIT License"
+            }
+        }
+        developers {
+            developer {
+                name = "Fudge"
+                url = "https://github.com/natanfudge"
+            }
+        }
+        scm {
+            url = githubUrl
+        }
+    }
+}
+
+
+
